6364f4111a08bc878182393a8c2f8b0847576e92
matheusps <matheus.ps@icloud.com>
2019-03-06 20:04:00 -0300

    Added tests for avatar, improve button logic for classes, adition of classnames lib

--- test/Avatar.test.js
+++ test/Avatar.test.js
@@ -2,8 +2,22 @@ import React from 'react'
 import Avatar from '../src/Components/Avatar'
 import renderer from 'react-test-renderer'
 
-test('Avatar Matches Snapshot', () => {
-  const component = renderer.create(<Avatar size="xl" chars="XL" />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+describe('Avatar', () => {
+  it('should match snapshot', () => {
+    const component = renderer.create(<Avatar size="xl" chars="XL" />).toJSON()
+    expect(component).toMatchSnapshot()
+  })
+
+  it('should render image correctly', () => {
+    const withImage = renderer
+      .create(<Avatar size="xl" image="testing" />)
+      .toJSON()
+    const withoutImage = renderer
+      .create(<Avatar size="xl" chars="XL" />)
+      .toJSON()
+
+    expect(withImage.children).toHaveLength(1)
+    expect(withImage.children[0].type).toEqual('img')
+    expect(withoutImage.children).toBeNull
+  })
 })
--- test/Button.test.js
+++ test/Button.test.js
@@ -2,8 +2,15 @@ import React from 'react'
 import Button from '../src/Components/Button'
 import renderer from 'react-test-renderer'
 
-test('Button Matches Snapshot', () => {
-  const component = renderer.create(<Button size="lg">Button Test</Button>)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+describe('Button', () => {
+  it('should match snapshot', () => {
+    const component = renderer
+      .create(
+        <Button size="lg" onClick={() => console.log('testing')}>
+          Button Test
+        </Button>
+      )
+      .toJSON()
+    expect(component).toMatchSnapshot()
+  })
 })
