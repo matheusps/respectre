7efbdeb7149f39a860e5d4fd36aa485f00af36bf
matheusps <matheus.ps@icloud.com>
2019-03-09 17:10:43 -0300

    Allow avatar to receive custom classes

--- src/Components/Avatar/index.js
+++ src/Components/Avatar/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-const Avatar = ({ size, background, image, chars }) => (
-  <figure
-    className={`avatar avatar-${size} bg-${background}`}
-    data-initial={chars}
-  >
-    {!!image && <img src={image} alt="Avatar" />}
-  </figure>
-)
+const Avatar = ({ size, background, image, chars, className, ...props }) => {
+  const avatarClasses = classNames(
+    className,
+    'avatar',
+    `avatar-${size}`,
+    `bg-${background}`
+  )
+  return (
+    <figure className={avatarClasses} data-initial={chars} {...props}>
+      {!!image && <img src={image} alt="Avatar" />}
+    </figure>
+  )
+}
 
 Avatar.defaultProps = {
   size: 'sm',
@@ -16,7 +22,9 @@ Avatar.defaultProps = {
 }
 
 Avatar.propTypes = {
+  /** Avatar size */
   size: PropTypes.oneOf(['xl', 'lg', 'sm', 'xs']),
+  /** Color of the background */
   background: PropTypes.oneOf([
     'primary',
     'secondary',
@@ -26,8 +34,12 @@ Avatar.propTypes = {
     'warning',
     'error',
   ]),
+  /** Image of the avatar */
   image: PropTypes.string,
+  /** Caracters to be shown if an image is not passed */
   chars: PropTypes.string,
+  /** Aditional css classes */
+  className: PropTypes.string,
 }
 
 export default Avatar
